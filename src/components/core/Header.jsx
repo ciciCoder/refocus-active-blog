b799192abbac348215c407dd5ba605fd5af30cca
Juncel G. Villar <cicivillar@gmail.com>
2023-07-07 14:20:01 +0800

    bug fixes

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -7,7 +7,9 @@ function Header() {
   return (
     <header className="Header bg-midnight-blue flex items-center h-auto md:h-[84px] pt-[40px] pb-[15px] px-[15px] md:p-0">
       <div className="flex items-center w-[90vw] xl:w-[1200px] mx-auto justify-between xl:px-[80px]">
-        <h1 className="logo">LOGO</h1>
+        <h1 className="logo">
+          <NavLink to="/">LOGO</NavLink>
+        </h1>
         <nav className="[display:none] md:flex w-[471px] justify-between">
           <ul className="flex items-center w-[227px] justify-between">
             <li>
